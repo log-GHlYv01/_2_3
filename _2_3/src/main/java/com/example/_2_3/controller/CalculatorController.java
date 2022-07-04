e66efebd519fec55302c4a578a147252c66cf8cd
LesnikovYV <Y-levl@ya.ru>
2022-07-04 20:08:35 +0500

    2.3.1

--- _2_3/src/main/java/com/example/_2_3/controller/CalculatorController.java
+++ _2_3/src/main/java/com/example/_2_3/controller/CalculatorController.java
@@ -4,13 +4,16 @@ import com.example._2_3.service.CalculatorService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.annotation.RequestScope;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/calculator")
 public class CalculatorController {
-        private final CalculatorServicev calculatorService;
+    private final CalculatorService calculatorService;
 
     public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
@@ -20,5 +23,44 @@ public class CalculatorController {
     public String greetings(){
         return "<h1>Вэлком в калькулятор </h1>";
     }
+    @GetMapping("/plue")
+    public String plue (@RequestParam(value = "num1", required = false) Integer a ,
+                        @RequestParam(value = "num2", required = false) Integer b){
+        if (Objects.isNull(a)|| Objects.isNull(b)){
+            return "точно все передано?";
+        }
+        return buildResult(a,b,calculatorService. plus (a,b), "+");
+    }
+    @GetMapping("/minus")
+    public String minus (@RequestParam(value = "num1", required = false) Integer a ,
+                         @RequestParam(value = "num2", required = false) Integer b){
+        if (Objects.isNull(a)|| Objects.isNull(b)){
+            return "точно все передано?";
+        }
+        return buildResult(a,b,calculatorService. minus  (a,b), "-");
+    }
+    @GetMapping("/minus")
+    public String multiply (@RequestParam(value = "num1", required = false) Integer a ,
+                            @RequestParam(value = "num2", required = false) Integer b){
+        if (Objects.isNull(a)|| Objects.isNull(b)){
+            return "точно все передано?";
+        }
+        return buildResult(a,b,calculatorService.multiply  (a,b), "*");
+    }
+    @GetMapping("/divide ")
+    public String divide (@RequestParam(value = "num1", required = false) Integer a ,
+                          @RequestParam(value = "num2", required = false) Integer b){
+        if (Objects.isNull(a)|| Objects.isNull(b)){
+            return "точно все передано?";
+        }
+        if (b==0) {
+            return "на ноль делить нельзя";
+        }
+        return buildResult(a,b,calculatorService.divide  (a,b), ":");
+    }
+    public String buildResult(Integer a ,Integer b , Number result,  String operation){
+        return a + " " + operation + " "+ b + " = "+ result ;
+    }
 
 }
+
--- _2_3/src/main/java/com/example/_2_3/service/CalculatorService.java
+++ _2_3/src/main/java/com/example/_2_3/service/CalculatorService.java
@@ -4,4 +4,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
+    public int plus(Integer a, Integer b ){
+        return a + b;
+    }
+
+    public Number minus(Integer a, Integer b) {
+        return a-b;
+    }
+
+    public Number multiply(Integer a, Integer b) {
+        return a*b;
+    }
+    public Number divide(Integer a, Integer b) {
+        return a/b;
+    }
 }
+
